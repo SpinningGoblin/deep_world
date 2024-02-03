792454877daef13c1d4a4d9d0175d75990395787
Derrick Plotsky <derrickplotsky@gmail.com>
2024-02-03 11:31:59 -0800

    Add a couple comments

--- app/core/src/rules/character_class.rs
+++ app/core/src/rules/character_class.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::DiceCount;
 
+/// The class that you will be able to level as your character levels.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CharacterClass {
     pub id: String,
--- app/core/src/rules/dice_count.rs
+++ app/core/src/rules/dice_count.rs
@@ -2,5 +2,6 @@ use serde::{Deserialize, Serialize};
 
 use super::DiceType;
 
+/// A count of dice to roll for any reason. e.g. 2d6
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
 pub struct DiceCount(u8, DiceType);
